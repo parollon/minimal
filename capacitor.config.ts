1452990801e253a095d7aa34accdc9e2ee48cb6a
Pere Rollon <parollon@useit.es>
2025-08-26 12:54:51 +0200

    minimal project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Minimal',
-  webDir: 'www'
+  webDir: 'www',
+    plugins: {
+    SafeArea: {
+      enabled: true,
+      customColorsForSystemBars: true,
+      statusBarColor: '#000000',
+      statusBarContent: 'light',
+      navigationBarColor: '#000000',
+      navigationBarContent: 'light',
+      offset: 0,
+    },
+  },
 };
 
 export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { SafeArea } from  '@capacitor-community/safe-area';
 
+SafeArea.enable({
+    config: {
+        /* same as what was in capacitor.config.ts */
+        //@ts-ignore
+        //Object literal may only specify known properties, and 'enabled' does not exist in type 'Config'.ts(2353)
+      enabled: true,
+      customColorsForSystemBars: true,
+      statusBarColor: '#000000',
+      statusBarContent: 'light',
+      navigationBarColor: '#000000',
+      navigationBarContent: 'light',
+      offset: 0,
+    }
+});
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
